05bfd1aaec287363b19f06de1cbf4a1feb9063b4
Beth <bethm.meeker@gmail.com>
2022-03-25 13:55:09 -0600

    push refactored controllers

--- src/main/java/com/projects/diningreviewsetup/Controller/DiningReviewController.java
+++ src/main/java/com/projects/diningreviewsetup/Controller/DiningReviewController.java
@@ -30,14 +30,6 @@ public class DiningReviewController {
         } else return Optional.empty();
     }
 
-    // returns review list with requested peanut score
-    @GetMapping("/peanut/score_{peanutScore}")
-    public List<DiningReview> getReviewByPeanutScore(@PathVariable("peanutScore") Integer peanutScore) {
-        if (peanutScore != null) {
-            return diningReviewRepository.findByPeanutScore(peanutScore);
-        } else return new ArrayList<>();
-    }
-
     // returns review list with requested min peanut score
     @GetMapping("/peanut/score_greaterthanequal_{peanutScore}")
     public List<DiningReview> getReviewsByPeanutScoreGreaterThanEqual(@PathVariable("peanutScore") Integer peanutScore) {
@@ -46,27 +38,11 @@ public class DiningReviewController {
         } else return new ArrayList<>();
     }
 
-    // returns review list with requested egg score
-    @GetMapping("/egg/score_{eggScore}")
-    public List<DiningReview> getReviewByEggScore(@PathVariable("eggScore") Integer eggScore) {
-        if (eggScore != null) {
-            return diningReviewRepository.findByEggScore(eggScore);
-        } else return new ArrayList<>();
-    }
-
     // returns review list with requested min egg score
     @GetMapping("/egg/score_greaterthanequal_{eggScore}")
     public List<DiningReview> getReviewsByEggScoreGreaterThanEqual(@PathVariable("eggScore") Integer eggScore) {
         if (eggScore != null) {
             return diningReviewRepository.findByEggScoreGreaterThanEqual(eggScore);
-        } else return new ArrayList<>();
-    }
-
-    // returns review list with requested dairy score
-    @GetMapping("/dairy/score_{dairyScore}")
-    public List<DiningReview> getReviewsByDairyScore(@PathVariable("dairyScore") Integer dairyScore) {
-        if (dairyScore != null) {
-            return diningReviewRepository.findByDairyScore(dairyScore);
         } else return new ArrayList<>();
     }
 
--- src/main/java/com/projects/diningreviewsetup/Repositories/DiningReviewRepository.java
+++ src/main/java/com/projects/diningreviewsetup/Repositories/DiningReviewRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface DiningReviewRepository extends CrudRepository<DiningReview, Long> {
-    List<DiningReview> findByPeanutScore(Integer peanutScore);
+
     List<DiningReview> findByPeanutScoreGreaterThanEqual(Integer peanutScore);
-    List<DiningReview> findByEggScore(Integer eggScore);
+
     List<DiningReview> findByEggScoreGreaterThanEqual(Integer eggScore);
-    List<DiningReview> findByDairyScore(Integer dairyScore);
+
     List<DiningReview> findByDairyScoreGreaterThanEqual(Integer dairyScore);
+
 }
